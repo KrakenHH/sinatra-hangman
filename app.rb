6694843ea1f1df70c3b61db85a570aab958004c0
krakenHH <krakenHH85@gmail.com>
2016-07-11 02:00:14 -0500

    updated css

--- app.rb
+++ app.rb
@@ -1,16 +1,13 @@
 require './hangman'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'shotgun'
 
 h = Hangman.new
 @answer_array_str = h.correct_guesses.join(' ')
-
 @guesses = h.guessed_array.join(' ')
 
 
 get '/' do
-  h.guess_letter_or_word(params[:guess]) 
-  @answer_array_str = h.correct_guesses.join(' ')
-  @guesses = h.guessed_array.join(' ')
-  erb :index
+  erb :welcome
 end
